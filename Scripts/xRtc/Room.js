2258341c9f7899db7564cfceb9f2b366fda9a150
Aliaksandr Shukaila <shukailo@gmail.com>
2013-07-12 12:56:20 +0300

    API redesigning in progress: Room object redesigning

--- Scripts/xRtc/Room.js
+++ Scripts/xRtc/Room.js
@@ -6,9 +6,10 @@
 	xrtc.Class(xrtc, 'Room', function Room(info, am, sc) {
 		var proxy = xrtc.Class.proxy(this),
 			logger = new xrtc.Logger(this.className),
+			hcEvents = xrtc.HandshakeController.events,
+			scEvents = xrtc.ServerConnector.events,
 			name = null,
 			participants = [],
-			isHandshakeSubscribed = false,
 			isServerConnectorSubscribed = false,
 			roomOptions = {},
 			roomInfo = {},
@@ -30,8 +31,7 @@
 
 			// todo: maybe will be better to rename this method to 'enter'
 			join: function (userName, options) {
-				subscribeOnServerConnectorEvents.call(this);
-				subscribeOnHandshakeControllerEvents.call(this);
+				subscribeToServerEvents.call(this);
 
 				// roomOptions initialization
 				xrtc.Class.extend(roomOptions, xrtc.Connection.settings.options);
@@ -52,6 +52,13 @@
 
 					serverConnector.connect(token);
 				});
+			},
+
+			leave: function () {
+				unsubscribeFromServerEvents.call(this);
+
+				name = null;
+				participants = [];
 			},
 
 			connect: function (participantId, connectionOptions) {
@@ -75,14 +82,6 @@
 				connection.startSession(participantId, connectionOptions);
 			},
 
-			leave: function () {
-				unsubscribeFromServerConnectorEvents.call(this);
-				unsubscribeFromHandshakeControllerEvents.call(this);
-
-				name = null;
-				participants = [];
-			},
-
 			getInfo: function ()
 			{
 				return roomInfo;
@@ -104,115 +103,54 @@
 			}
 		});
 
-		function subscribeOnServerConnectorEvents() {
+		function subscribeToServerEvents() {
 			if (!isServerConnectorSubscribed) {
 				serverConnector
-					.on(xrtc.ServerConnector.events.connectionOpen, proxy(onConnectionOpen))
-					.on(xrtc.ServerConnector.events.connectionClose, proxy(onConnectionClose))
-					.on(xrtc.ServerConnector.events.tokenInvalid, proxy(onTokenInvalid))
-					.on(xrtc.ServerConnector.serverEvents.participantsUpdated, proxy(onParticipantsUpdated))
-					.on(xrtc.ServerConnector.serverEvents.participantConnected, proxy(onParticipantConnected))
-					.on(xrtc.ServerConnector.serverEvents.participantDisconnected, proxy(onParticipantDisconnected));
+					.on(scEvents.connectionOpen, proxy(function (event) { this.trigger(xrtc.Room.events.join); }))
+					.on(scEvents.connectionClose, proxy(function (event) { this.trigger(xrtc.Room.events.leave); }))
+					.on(scEvents.tokenInvalid, proxy(function (event) { this.trigger(xrtc.Room.events.tokenInvalid); }))
+					.on(scEvents.participantsUpdated, proxy(function (data) {
+						name = data.room;
+						participants = data.connections;
+						sortParticipants();
+
+						this.trigger(xrtc.Room.events.participantsUpdated, { participants: this.getParticipants() });
+					}))
+					.on(scEvents.participantConnected, proxy(function (data) {
+						name = data.room;
+						participants.push(data.participantId);
+						sortParticipants();
+
+						this.trigger(xrtc.Room.events.participantConnected, { participantId: data.participantId });
+					}))
+					.on(scEvents.participantDisconnected, proxy(function (data) {
+						name = data.room;
+						participants.splice(participants.indexOf(data.participantId), 1);
+						sortParticipants();
+
+						this.trigger(xrtc.Room.events.participantDisconnected, { participantId: data.participantId });
+					}))
+					.on(scEvents.receiveOffer, proxy(onIncomingConnection));
 
 				isServerConnectorSubscribed = true;
 			}
 		}
 
-		function unsubscribeFromServerConnectorEvents() {
+		function unsubscribeFromServerEvents() {
 			if (isServerConnectorSubscribed) {
 				serverConnector
-					.off(xrtc.ServerConnector.serverEvents.participantsUpdated)
-					.off(xrtc.ServerConnector.serverEvents.participantConnected)
-					.off(xrtc.ServerConnector.serverEvents.participantDisconnected);
+					.off(scEvents.participantsUpdated)
+					.off(scEvents.participantConnected)
+					.off(scEvents.participantDisconnected);
+				
+				// todo: what about another events? (connectionOpen, connectionClose, tokenInvalid). Need to think about it later.
 
 				isServerConnectorSubscribed = false;
 			}
 		}
 
-		function onParticipantsUpdated(data) {
-			name = data.room;
-			participants = data.connections;
-			orderParticipants();
-
-			this.trigger(xrtc.Room.events.participantsUpdated, { participants: this.getParticipants() });
-		}
-
-		function onParticipantConnected(data) {
-			name = data.room;
-			participants.push(data.participantId);
-			orderParticipants();
-
-			this.trigger(xrtc.Room.events.participantConnected, { participantId: data.participantId });
-		}
-
-		function onParticipantDisconnected(data) {
-			name = data.room;
-			participants.splice(participants.indexOf(data.participantId), 1);
-			orderParticipants();
-
-			this.trigger(xrtc.Room.events.participantDisconnected, { participantId: data.participantId });
-		}
-
-		function orderParticipants() {
+		function sortParticipants() {
 			participants.sort();
-		}
-
-		function onConnectionOpen(event) {
-			this.trigger(xrtc.Room.events.join);
-		}
-
-		function onConnectionClose(event) {
-			this.trigger(xrtc.Room.events.leave);
-		}
-		
-		function onTokenInvalid(event) {
-			this.trigger(xrtc.Room.events.tokenInvalid, event);
-		}
-
-		function subscribeOnHandshakeControllerEvents() {
-			if (handshakeController && !isHandshakeSubscribed) {
-				var hcEvents = xrtc.HandshakeController.events;
-				handshakeController
-					.on(hcEvents.sendIce, proxy(onHandshakeSendMessage))
-					.on(hcEvents.sendOffer, proxy(onHandshakeSendMessage))
-					.on(hcEvents.sendAnswer, proxy(onHandshakeSendMessage))
-					.on(hcEvents.sendBye, proxy(onHandshakeSendMessage));
-
-				serverConnector
-					.on(hcEvents.receiveIce, proxy(onHandshakeReceiveMessage, hcEvents.receiveIce))
-					.on(hcEvents.receiveOffer, proxy(onHandshakeReceiveMessage, hcEvents.receiveOffer))
-					.on(hcEvents.receiveAnswer, proxy(onHandshakeReceiveMessage, hcEvents.receiveAnswer))
-					.on(hcEvents.receiveBye, proxy(onHandshakeReceiveMessage, hcEvents.receiveBye));
-
-				isHandshakeSubscribed = true;
-			}
-		}
-
-		function unsubscribeFromHandshakeControllerEvents() {
-			if (handshakeController && isHandshakeSubscribed) {
-				var hcEvents = xrtc.HandshakeController.events;
-				handshakeController
-					.off(hcEvents.sendIce)
-					.off(hcEvents.sendOffer)
-					.off(hcEvents.sendAnswer)
-					.off(hcEvents.sendBye);
-
-				serverConnector
-					.off(hcEvents.receiveIce)
-					.off(hcEvents.receiveOffer)
-					.off(hcEvents.receiveAnswer)
-					.off(hcEvents.receiveBye);
-
-				isHandshakeSubscribed = false;
-			}
-		}
-
-		function onHandshakeSendMessage(data) {
-			serverConnector.send(data);
-		}
-
-		function onHandshakeReceiveMessage(data, event) {
-			handshakeController.trigger(event, data);
 		}
 
 		function getConnectionIndexById(connectionsArray, connectionId) {
@@ -227,6 +165,10 @@
 			return resultIndex;
 		}
 		
+		function onIncomingConnection(data) {
+			// ???
+		}
+
 		function createConnection(userData, participantId) {
 			var hc = new xrtc.HandshakeController();
 
@@ -236,10 +178,10 @@
 			handshakeControllers[connectionId] = hc;
 
 			var eventsMapping = {};
-			eventsMapping[xrtc.ServerConnector.events.receiveOffer] = xrtc.HandshakeController.events.receiveIce;
-			eventsMapping[xrtc.ServerConnector.events.receiveAnswer] = xrtc.HandshakeController.events.receiveAnswer;
-			eventsMapping[xrtc.ServerConnector.events.receiveIce] = xrtc.HandshakeController.events.receiveIce;
-			eventsMapping[xrtc.ServerConnector.events.receiveBye] = xrtc.HandshakeController.events.receiveBye;
+			eventsMapping[scEvents.receiveOffer] = hcEvents.receiveOffer; // ???
+			eventsMapping[scEvents.receiveAnswer] = hcEvents.receiveAnswer;
+			eventsMapping[scEvents.receiveIce] = hcEvents.receiveIce;
+			eventsMapping[scEvents.receiveBye] = hcEvents.receiveBye;
 			for (var eventName in eventsMapping) {
 				if (eventsMapping.hasOwnProperty(eventName)) {
 					serverConnector.on(event, function (data) {
@@ -251,6 +193,13 @@
 						}
 					});
 				}
+			}
+
+			var hcSendEvents = [hcEvents.sendOffer, hcEvents.sendAnswer, hcEvents.sendIce, hcEvents.sendBye];
+			for (var i = 0, len = hcSendEvents.length; i < len; i++) {
+				hc.on(hcSendEvents[i], proxy(function (data) {
+					serverConnector.send(data);
+				}));
 			}
 
 			self.trigger(xrtc.Room.events.connectionCreated, { participantId: participantId, connection: connection });
@@ -284,7 +233,7 @@
 			participantsUpdated: 'participantsupdated',
 			participantConnected: 'participantconnected',
 			participantDisconnected: 'participantdisconnected',
-			tokenInvalid: 'tokenexpired'
+			tokenInvalid: 'tokeninvalid'
 		},
 
 		settings: {
