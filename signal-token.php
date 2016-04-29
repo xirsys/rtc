d1ee16a55d282ccce8d612fff9ea11a7f2474afb
Lee Sylvester <lee.sylvester@touchtech.co.nz>
2016-04-30 09:51:26 +1200

    Add moved files

---
+++ signal-token.php
@@ -0,0 +1,41 @@
+<?php
+//set POST variables
+$url = 'https://service.xirsys.com/signal/token';
+$fields_string = '';
+$fields = array(
+	'domain' => $_POST["domain"],
+	'application' => $_POST["application"],
+	'room' => $_POST["room"],
+	'username' => $_POST["username"],
+	'ident' => urlencode("your_user_name"),
+	'secret' => urlencode("your-uuid-secret-key"),
+	"secure" => "1" 
+);
+
+//url-ify the data for the POST
+foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+rtrim($fields_string, '&');
+
+//open connection
+$ch = curl_init();
+
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, count($fields));
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch,CURLOPT_TIMEOUT, 0);
+curl_setopt($ch,CURLOPT_CONNECTTIMEOUT, 0);
+
+//execute post
+$result = curl_exec($ch);
+
+// Check for errors
+if($result === FALSE){
+    die(curl_error($ch));
+}
+
+//close connection
+curl_close($ch);
+
+?>
